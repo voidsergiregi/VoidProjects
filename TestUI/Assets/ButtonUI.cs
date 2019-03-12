8e163c6cd3ff7dcea4c09343a7ab60c31f115c7f
Sergi Regi C <sergiregi88@gmail.com>
2019-03-12 12:18:48 +0100

    aadded unity test

---
+++ TestUI/Assets/ButtonUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUI : MonoBehaviour {
+    public Text TextButton;
+    public Image imageButton;
+    public bool isShowing;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.S))
+            ShowHideButton();
+	}
+    public void ShowHideButton()
+    {
+        isShowing = !isShowing;
+        TextButton.enabled = isShowing;
+        imageButton.enabled = isShowing;
+       
+    }
+   
+
+}
---
+++ TestUI/Assets/FuntionsButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuntionsButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void FuntionButton1()
+    {
+        Debug.Log("funtion of button 1 executing");
+    }
+    public void FuntionButton2()
+    {
+        Debug.Log("funtion of button 2 executing");
+    }
+    public void FuntionButton3()
+    {
+        Debug.Log("funtion of button 3 executing");
+    }
+    public void FuntionButton4()
+    {
+        Debug.Log("funtion of button 4 executing");
+    }
+    public void FuntionButton5()
+    {
+        Debug.Log("funtion of button 5 executing");
+    }
+    public void FuntionButton6()
+    {
+        Debug.Log("funtion of button 6 executing");
+    }
+}
