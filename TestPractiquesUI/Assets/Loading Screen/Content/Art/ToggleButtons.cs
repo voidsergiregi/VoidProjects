86da96c09cb721d5a6263d865c664e50000633a7
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2019-03-12 09:14:49 +0100

    initcommit

---
+++ TestPractiquesUI/Assets/Loading Screen/Content/Art/ToggleButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButtons : MonoBehaviour {
+    public List<Button> listButton;
+    public Button currentButton;
+    public Button startButton;
+    public SpriteState sprState;  
+    //public SpriteState SelectionState;
+
+    // Use this for initialization
+    void Start () {
+        listButton.AddRange(GetComponentsInChildren<Button>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)){
+            SwitchActiveButton(listButton[2]);
+        }
+	}
+    public void SwitchActiveButton(Button button)
+    {
+        if (button)
+        {
+            if (currentButton)
+            {
+                currentButton.spriteState = sprState; 
+            }
+            currentButton = button;
+           
+        }
+    }
+}
